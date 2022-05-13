e8df0bcd803a6609c731e51ba2af11110dba5279
unknown <salimrafaelcastro.ortiz@gmail.com>
2022-05-13 13:27:47 -0500

    Se agrego proyecto a Git

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,23 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Stard Lord', history: 'Star-Lord es un personaje ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. El personaje, creado por Steve Englehart y Steve Gan, apareció por primera vez en Marvel', imagen: '../assets/images/star.jpg', poderes: 'rayo laser' },
+  { id: 12, name: 'Groot', history: 'root es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. Creado por Stan Lee, Larry Lieber ', imagen: '../assets/images/groot.jpg', poderes: 'rayo laser' },
+
+  { id: 13, name: 'Hulk', history: 'Hulk es un personaje ficticio, un superhéroe que aparece en los cómics estadounidenses publicados por la editorial Marvel Comics. El personaje fue creado por los escritores Stan Lee y Jack Kirby siendo su primera aparición en The Incredible Hulk #1', imagen: '../assets/images/hulk.jpg', poderes: 'rayo laser' },
+
+  { id: 14, name: 'Capitan America', history: 'El Capitán América, cuyo nombre real es Steven "Steve" Grant Rogers, es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics', imagen: '../assets/images/capi.jpg', poderes: 'rayo laser' },
+
+  { id: 15, name: 'Iron Man', history: 'ron Man es un superhéroe que aparece en los cómics estadounidenses publicados por Marvel Comics. El personaje fue cocreado por el escritor y editor Stan Lee, desarrollado por el guionista Larry Lieber y diseñado por los artistas Don Heck y Jack Kirby.', imagen: '../assets/images/iron.jpg', poderes: 'rayo laser' },
+
+  { id: 16, name: 'Ant Man', history: 'Ant-Man (Hombre Hormiga en algunas traducciones al español) es el nombre de varios superhéroes ficticios que aparecen en libros publicados por Marvel Comics. Creado por Stan Lee, Larry Lieber y Jack Kirby, la primera aparición de Ant-Man fue en el número 27 de Tales to Astonish (de enero de 1962).', imagen: '../assets/images/anti.jpg', poderes: 'rayo laser' },
+
+  { id: 17, name: 'Benito Juarez', history: 'Benito Pablo Juárez García, fue un abogado y político mexicano, de origen indígena, presidente de México en varias ocasiones, del 21 de enero de 1858 al 18 de julio de 1872.​ Se le conoce como el «Benemérito de las Américas', imagen: '../assets/images/benito.jpg', poderes: 'rayo laser' },
+
+  { id: 18, name: 'Thor', history: 'Thor es un personaje importante en el Universo Cinematográfico Marvel, siendo el único Vengador que no es de la Tierra. Thor empuña el martillo de guerra místico Mjolnir, que controla el clima, pero él también tiene fuerza, durabilidad y agilidad similares a las de un dios.', imagen: '../assets/images/thor.jpg', poderes: 'rayo laser' },
+
+  { id: 19, name: 'Odin', history: 'Odín fue un personaje ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics  Odin un antiguo personaje originario de la mitología Nórdica, aunque como Jens Peter Schjødt nos señala, muchos academicos piensan que su alta posición en la jerarquía de los dioses podría haber sido añadida mucho más tarde (219). El rol de Odin como "Padre de todo" o padre de los dioses es un tema literario abordado en fuentes posteriores -aparentemente influenciados por nombres Cristianos a Dios- que a una real reflexión de su estatus en las sociedades de la Era Vikinga. La poesía skaldic (Era Vikinga, poesía pre-Cristiana mayoritariamente oídas en cortes de reyes y sus séquitos), por ejemplo, nombres como Balder, Thor, y Vali como hijos de Odin, mientras que mucho después en el Siglo XIII el autor Islandés Snorri Sturluson también incluye a Heimdall, Týr, Bragi, Vidarr, y Hodr a la lista. Y aunque Odin se encontraba casado a Frigg, muchos de estos hijos provienen de diferentes madres, Odin aparece como un mujeriego en muchos de sus relatos e incluso presumiendo de estos amorios, recordandonos de (y quizás inspirado por) Zeus de la mitología Griega.', imagen: '../assets/images/odin.jpg', poderes: 'rayo laser' },
+
+  { id: 20, name: 'Doctor Strange',history:'En DOCTOR STRANGE EN EL MULTIVERSO DE LA LOCURA de Marvel Studios, el Universo Cinematográfico de Marvel (MCU por sus siglas en inglés) se adentra en el Multiverso y amplía sus límites más que nunca. El film presenta un viaje a lo desconocido con Doctor Strange, quien, con la ayuda de aliados místicos nuevos y otros ya conocidos por la audiencia, atraviesa las alucinantes y peligrosas realidades alternativas del Multiverso para enfrentarse a un nuevo y misterioso adversario.',imagen:'../assets/images/doctor.jpg',poderes:'rayo laser' },
+
+];
